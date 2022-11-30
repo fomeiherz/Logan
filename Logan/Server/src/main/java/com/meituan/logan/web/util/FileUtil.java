369f1e75fa5a8e28ee6873e7982e601a807f0b73
fomeiherz <fomeiherz@users.noreply.github.com>
2022-11-30 10:45:09 +0800

    1. Change upload file path. 2. Change log file path.

--- Logan/Server/src/main/java/com/meituan/logan/web/util/FileUtil.java
+++ Logan/Server/src/main/java/com/meituan/logan/web/util/FileUtil.java
@@ -1,6 +1,7 @@
 package com.meituan.logan.web.util;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -11,6 +12,8 @@ import java.io.IOException;
  * * @since 2019-11-25 17:01
  */
 public class FileUtil {
+
+    private static final Logger LOGGER = Logger.getLogger(FileUtil.class);
 
     public static File createNewFile(String fileName) {
         File file = getFile(fileName);
@@ -23,19 +26,23 @@ public class FileUtil {
         if (file.exists()) {
             file.delete();
         }
+        LOGGER.info("Try create file: " + file.getAbsolutePath());
         try {
             file.createNewFile();
         } catch (IOException e) {
+            LOGGER.error(e);
             return null;
         }
         return file;
     }
 
     public static File getFile(String fileName) {
-        String path = new File("").getAbsolutePath() + File.separator + "logfile" + File.separator;
+//        String path = new File("").getAbsolutePath() + File.separator + "logfile" + File.separator;
+        String path = "/www/wwwlogs" + File.separator + "logfile" + File.separator;
         File file = new File(path + fileName);
         if (!path.equals(file.getParentFile().getAbsolutePath()+ File.separator)) {
-            return null;
+            LOGGER.warn(path + " != " + file.getParentFile().getAbsolutePath()+ File.separator);
+            //return null;
         }
         return file;
     }
